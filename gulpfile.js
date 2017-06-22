8a56c18e0859051a8accfd8b7b16533682e38fb7
Jenia <jeniabuianov@gmail.com>
2017-06-22 14:26:32 +0300

    Changed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,12 @@
 var gulp = require('gulp');
 // Requires the gulp-sass plugin
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
     return gulp.src('public/assets/custom/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
         .pipe(sass())
         .pipe(gulp.dest('public/assets/custom/css/'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+       
 });
 
 gulp.task('default', function(){
